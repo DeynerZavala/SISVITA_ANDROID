3e1184f25b1c12ac5f4b38d9c3dd323eb07457f8
deyner <dyr.zva.hyi@gmail.com>
2024-06-14 20:45:13 -0500

    CUS2

--- app/src/main/java/com/example/sisvita_android/MainActivity.kt
+++ app/src/main/java/com/example/sisvita_android/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.sisvita_android
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,6 +16,7 @@ import com.example.sisvita_android.navigation.AppNavigation
 import com.example.sisvita_android.ui.theme.SISVITA_ANDROIDTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -31,6 +34,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
